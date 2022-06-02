7fa0c4b15bd3b553363b69b4f3538fa6703da306
Sarah Menu <sarah.menu@gmail.com>
2022-06-02 19:56:57 +0200

    Working on tapbar

---
+++ app/javascript/controllers/tabbar_controller.js
@@ -0,0 +1,26 @@
+import { Controller } from "@hotwired/stimulus"
+
+
+export default class extends Controller {
+  static targets = ["alert", "search"]
+
+  connect() {
+    // console.log(this.alertLinkTarget);
+  }
+
+  redirectToAlertPage() {
+    document.location.href = "../my_garden/user_plants/needing_attention"
+  }
+
+  redirectToSearchPage(event) {
+    document.location.href = "../plants"
+    // console.log("yoyoyoyoyoyoyoyoyoyoyoy je suis un truc qui marche pas")
+    this.searchTarget.setAttribute("checked", true)
+    event.preventDefault()
+    // event.stopPropagation()
+  }
+
+  redirectToGardenPage(event) {
+    document.location.href = "../my_garden/user_plants"
+  }
+}
