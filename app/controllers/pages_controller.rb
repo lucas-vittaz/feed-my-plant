ebc5c54b188d51f718f9edf013608ad0da810a7f
lucas vittaz <lucasvittaz.pro@gmail.com>
2022-07-27 14:27:07 +0200

    backend badge notification wip

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    @plants_attention = UserPlant.select{|user_plant| (user_plant.latest_hygrometry < user_plant.plant.min_baseline_hygrometry ||user_plant.latest_hygrometry > user_plant.plant.max_baseline_hygrometry)}
   end
 end
